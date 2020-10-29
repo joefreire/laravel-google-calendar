0e0a0000cd6b836bb53a233d1d06c8ebe18fed54
Guilherme Freire <freire.joe@gmail.com>
2020-10-28 22:52:45 -0300

    Update Event.php

--- src/Event.php
+++ src/Event.php
@@ -38,17 +38,19 @@ class Event
     public static function createFromGoogleCalendarEvent(Google_Service_Calendar_Event $googleEvent, $calendarId)
     {
         //this option are to create a conference and add a link to meet in event
-        $googleCalendar = static::getGoogleCalendar($calendarId);
-        if($calendarId == null){
-            $calendarId =$googleCalendar->getCalendarId();
+        if(empty($googleEvent->hangoutLink)){
+            $googleCalendar = static::getGoogleCalendar();
+            if($calendarId == null){
+                $calendarId =$googleCalendar->getCalendarId();
+            }
+            $service = $googleCalendar->getService();
+            $conference = new \Google_Service_Calendar_ConferenceData();
+            $conferenceRequest = new \Google_Service_Calendar_CreateConferenceRequest();
+            $conferenceRequest->setRequestId('randomString123');
+            $conference->setCreateRequest($conferenceRequest);
+            $googleEvent->setConferenceData($conference);
+            $googleEvent = $service->events->patch($calendarId, $googleEvent->id, $googleEvent, ['conferenceDataVersion' => 1]);
         }
-        $service = $googleCalendar->getService();
-        $conference = new \Google_Service_Calendar_ConferenceData();
-        $conferenceRequest = new \Google_Service_Calendar_CreateConferenceRequest();
-        $conferenceRequest->setRequestId('randomString123');
-        $conference->setCreateRequest($conferenceRequest);
-        $googleEvent->setConferenceData($conference);
-        $googleEvent = $service->events->patch($calendarId, $googleEvent->id, $googleEvent, ['conferenceDataVersion' => 1]);
 
         $event = new static;
 
